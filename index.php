8fd864116a886224f45b092825fc4044b171dd0e
Workbeesdig <adlen025@gmail.com>
2018-03-01 20:02:33 +0100

    coming soon

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-<h3>Website under construction</h3>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Coming Soon</title>
+    <link rel="stylesheet" href="./assets/style/style.css">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+</head>
+
+<body>
+    <div class="valign-wrapper purple darken-3">
+        <h1 class="center-align row white-text">
+            Coming soon
+        </h1>
+    </div>
+</body>
+
+</html>
